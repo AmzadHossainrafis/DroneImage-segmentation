ea3924cb9df50d9a85dbdcbce5657c23743916cb
Amzad hossain <amzad.rafi@northsouth.edu>
2023-11-06 02:00:13 +0600

    --train val tqdm emplimented

--- src/DronVid/components/trainer.py
+++ src/DronVid/components/trainer.py
@@ -1,12 +1,11 @@
-from utils.common import * 
-import torch
-import tqdm
-import logging
 from tqdm import tqdm 
+import torch
+from DronVid.components.utils.logger import logger
+from DronVid.components.utils.common import read_yaml
 
 
 
-config = read_yaml("../../../config/config.yaml")   
+config = read_yaml("config/config.yaml")   
 
 
 '''
@@ -19,100 +18,67 @@ Bug report:
 
 
 '''
-config = read_yaml("../../../config/config.yaml")
-logger = logging.getLogger(__name__)
 
 
-class Trainer(object): 
-    """
-    Trainer class for training the model, saving the model, logging the training and validation loss
-
-    args:
-        batch_size: batch size for training 
-        model: model to train 
-        optimizer: optimizer for training 
-        scheduler: scheduler for training 
-        train_loss: list of training loss 
-        val_loss: list of validation loss 
-        device: cpu or gpu
-
-    return:
-        train_loss: list of training loss 
-        val_loss: list of validation loss
-    
-    
-    """
 
 
-    def __init__(self, batch_size , model, auto_save=False) -> None:
-        super().__init__()
-        self.batch_size = batch_size
+
+class Trainer:
+    def __init__(self, model, loss, optimizer, device):
         self.model = model
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        self.model.to(self.device)
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.001)
-        self.auto_save = auto_save
-        self.train_config = config["Train_config"]
-        
+        self.criterion = loss
+        self.optimizer = optimizer
+        self.device = device
+        self.best_val_loss = float('inf')
 
-    def train(train_dl, val_dl,model,loss,optimizer):
-        try : 
-            for epoch in range(self.train_config["epochs"]):
-                train_loss = []
-                val_loss = []
-                print("Training Started...")
-                logger.info(f"Training Started for epoch {epoch+1}")
-                for epoch in tqdm(range(self.train_config["epochs"])):
-                    print(f"Epoch {epoch+1} of {self.train_config['epochs']}")
-                    model.train()
-                    running_loss = 0.0
-                    for x, y in tqdm(train_dl):
-                        x = x.to("cuda")
-                        y = y.to("cuda")
-                        optimizer.zero_grad()
-                        y_hat = model(x)
-                        loss = loss(y_hat, y)
-                        loss.backward()
-                        optimizer.step()
-                        running_loss += loss.item()
-                    epoch_loss = running_loss / len(train_dl)
-                    train_loss.append(epoch_loss)
-                    print(f"Train Loss: {epoch_loss:.4f}")
-                    
-                    model.eval()
-                    running_loss = 0.0
-                    for x, y in val_dl:
-                        x = x.to("cuda")
-                        y = y.to("cuda")
-                        with torch.no_grad():
-                            y_hat = model(x)
-                            loss = loss(y_hat, y)
-                            running_loss += loss.item()
+    def train_step(self, dataloader):
+        self.model.train()
+        total_loss = 0.0
+        pbar = tqdm(enumerate(dataloader), total=len(dataloader), desc="Training")
+        for i, (x, y) in pbar:
+            x = x.to(self.device)
+            y = y.to(self.device)
+            self.optimizer.zero_grad()
+            y_hat = self.model(x)
+            loss = self.criterion(y_hat, y)
+            loss.backward()
+            self.optimizer.step()
+            total_loss += loss.item()
+            pbar.set_postfix({'loss': total_loss / (i + 1)})
+        pbar.close()
 
-                    logger.info(f"Epoch {epoch+1} - Validation Loss: {epoch_loss:.4f}")
-                    epoch_loss = running_loss / len(val_dl)
-                    val_loss.append(epoch_loss)
-                    print(f"Val Loss: {epoch_loss:.4f}")
-            
+    def validate_step(self, dataloader):
+        self.model.eval()
+        total_loss = 0.0
+        pbar = tqdm(enumerate(dataloader), total=len(dataloader), desc="Validation")
+        with torch.no_grad():
+            for i, (x, y) in pbar:
+                x = x.to(self.device)
+                y = y.to(self.device)
+                y_hat = self.model(x)
+                loss = self.criterion(y_hat, y)
+                total_loss += loss.item()
+                pbar.set_postfix({'loss': total_loss / (i + 1)})
+        pbar.close()
+        return total_loss / len(dataloader)
 
-            if self.auto_save:
-                torch.save(model.state_dict(), config["artifacts"]["model_dir"] + "model.pt")
-                logger.info("Model saved successfully")
-                print("Model saved successfully")
+    def save_model(self, path):
+        torch.save(self.model.state_dict(), path)
 
-                return train_loss, val_loss 
-            else:
-                return train_loss, val_loss
-        
+    def train(self, train_dl, val_dl, epochs, model_path):
+        for epoch in range(epochs):
+            self.train_step(train_dl)
+            val_loss = self.validate_step(val_dl)
+            if val_loss < self.best_val_loss:
+                self.best_val_loss = val_loss
+                self.save_model(model_path)
+                logger.info("Saving model with lowest loss: {}".format(val_loss))
+            logger.info("Epoch: {} | val_loss: {}".format(epoch, val_loss))
+            logger.info("Best val loss: {}".format(self.best_val_loss))
+            logger.info("--------------------------------------------------")
 
-        
-        except Exception as e:
-            logger.error("Error in training the model")
-            print(e)
-      
-
-
-
+  
+       
         
         
 
